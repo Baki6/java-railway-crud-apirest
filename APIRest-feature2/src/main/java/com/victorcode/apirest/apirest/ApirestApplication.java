b9101e58de955356caddca7f12ec2b79c3082bac
Victor Moctezuma <victorvmm10@gmail.com>
2024-09-14 23:01:15 -0600

    Hicimos una versión configurada de nuestro proyecto APIREST CRUD con POSTGRES para poder desplegar en Railway

---
+++ APIRest-feature2/src/main/java/com/victorcode/apirest/apirest/ApirestApplication.java
@@ -0,0 +1,13 @@
+package com.victorcode.apirest.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // hace la configuración inicial por defecto de Spring Boot
+public class ApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestApplication.class, args);
+	}
+
+}
---
+++ APIRest-feature2/src/main/java/com/victorcode/apirest/apirest/Repositories/ProductoRepository.java
@@ -0,0 +1,10 @@
+package com.victorcode.apirest.apirest.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.victorcode.apirest.apirest.Entities.Producto;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+
+
+}
